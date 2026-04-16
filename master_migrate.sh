5b40b86fce9500d19f610e923ebfb3294a924322
Andrew Mikhail <andrewmikhail@ymail.com>
2026-04-16 16:08:05 -0700

    Fixed typo

--- master_migrate.sh
+++ master_migrate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # master_deploy.sh
 
-sudo bash make_partitions.sh
-sudo bash copy_partitions.sh
+sudo bash ./make_partitions.sh
+sudo bash ./copy_partitions.sh
 sync
-sudo bash configure_ssd_boot.sh
-sudo bash fix_boot.sh
-sudo bash setup_storage.sh
+sudo bash ./configure_ssd_boot.sh
+sudo bash ./fix_boot.sh
+sudo bash ./setup_storage.sh
